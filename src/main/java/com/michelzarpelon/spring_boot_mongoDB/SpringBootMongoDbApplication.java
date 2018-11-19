44cc59d82fc75499b1119bf931c856d675d5bf99
michelzarpe <michelzarpe@gmail.com>
2018-11-19 09:59:07 -0200

    criando projeto

---
+++ src/main/java/com/michelzarpelon/spring_boot_mongoDB/SpringBootMongoDbApplication.java
@@ -0,0 +1,12 @@
+package com.michelzarpelon.spring_boot_mongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongoDbApplication.class, args);
+	}
+}

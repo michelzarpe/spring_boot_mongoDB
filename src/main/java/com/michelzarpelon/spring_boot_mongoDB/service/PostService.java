8051dcb63a0e5b556d41e4b2e5f5ff5be1574c6d
michelzarpe <michelzarpe@gmail.com>
2018-11-20 10:54:54 -0200

    Consulta simples com @Query em mongoDB

--- src/main/java/com/michelzarpelon/spring_boot_mongoDB/service/PostService.java
+++ src/main/java/com/michelzarpelon/spring_boot_mongoDB/service/PostService.java
@@ -22,7 +22,7 @@ public class PostService {
 	}
 
 	public List<Post> findByTitle(String text){
-		return objRepository.findByTitleContainingIgnoreCase(text);
+		return objRepository.findByTitle(text);
 	}
 
 }
